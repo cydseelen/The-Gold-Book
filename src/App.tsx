edd48ca3b63aff78c4aa388ae82e77b2209eee75
Cydnie Seelenbinder <cydalexand@gmail.com>
2022-06-11 23:33:23 +0200

    Removed logo images, defined custom theme, and installed React Router library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
 import React from "react";
-import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { Box, CssBaseline, Paper, ThemeProvider, Typography } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#63b8ff",
+        main: "#0989e3",
+        dark: "#005db0",
+        contrastText: "#000",
+      },
+      secondary: {
+        main: "#4db6ac",
+        light: "#82e9de",
+        dark: "#00867d",
+        contrastText: "#000",
+      },
+    },
+  });
+
   return (
-    <div>
-      <CssBaseline />
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Paper
-          elevation={3}
-          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box
+          height="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
         >
-          <Typography color="primary.dark" variant="h1">
-            Starter App
-          </Typography>
-        </Paper>
-      </Box>
-    </div>
+          <Paper
+            elevation={3}
+            sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+          >
+            <Typography color="primary.dark" variant="h1">
+              The Gold Book
+            </Typography>
+          </Paper>
+        </Box>
+      </ThemeProvider>
   );
 }
 
